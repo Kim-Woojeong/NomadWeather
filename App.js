355516b0b8ae21f96e555dad4b0d8206f559c23a
Kim-Woojeong <kyj990307@naver.com>
2022-11-06 19:30:00 +0900

    1.5 Styles

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-       <View style={{flex: 1, backgroundColor : "tomato"}}></View>
-       <View style={{flex: 2, backgroundColor : "teal"}}></View>
-       <View style={{flex: 1, backgroundColor : "orange"}}></View>
-    </View>
+       <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <View style={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>Sunny</Text>
+          </View>
+        </View>
+       </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor : "tomato",
+  },
+  city : {
+    flex: 1.2,
+    backgroundColor : "tomato",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather : {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "teal",
+  },
+  temp : {
+    fontSize: 170,
+    marginTop: 50
+  },
+  description : {
+    fontSize: 60,
+    marginTop: -30
+  }
+})
